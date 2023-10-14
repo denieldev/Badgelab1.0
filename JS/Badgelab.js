9e6d8233ff932d023b670e931f01cdd2d748ec01
Victor Daniel Martins Feitosa <103947188+denieldev@users.noreply.github.com>
2023-10-14 18:47:27 -0300

    button open and close

--- JS/Badgelab.js
+++ JS/Badgelab.js
@@ -1,9 +1,13 @@
 var display = document.getElementById("help_container");
 
-function Mudarestado() {
+function abrirDiv() {
   var display = document.getElementById("help_container").style.display;
-  if(display == "none")
+ display == "none";
       document.getElementById("help_container").style.display = 'flex';
-  else
-      document.getElementById("help_container").style.display = 'none';
 }
+function fecharDiv() {
+    var display = document.getElementById("help_container").style.display;
+    display == "flex";
+        document.getElementById("help_container").style.display = 'none';
+  }
+
