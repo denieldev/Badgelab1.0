9861420346fbe68a4a72a17d8b17e3406f8f6bef
Daniel Martins <103947188+denieldev@users.noreply.github.com>
2024-01-11 11:57:12 -0300

    blur in help window

--- JS/Badgelab.js
+++ JS/Badgelab.js
@@ -2,12 +2,15 @@ var display = document.getElementById("help_container");
 
 function abrirDiv() {
   var display = document.getElementById("help_container").style.display;
- display == "none";
-      document.getElementById("help_container").style.display = 'flex';
+  display == "none";
+  document.getElementById("help_container").style.display = 'flex';
+  document.getElementById("blur-background").style.display = 'block';
 }
+
 function fecharDiv() {
-    var display = document.getElementById("help_container").style.display;
-    display == "flex";
-        document.getElementById("help_container").style.display = 'none';
-  }
+  var display = document.getElementById("help_container").style.display;
+  display == "flex";
+  document.getElementById("help_container").style.display = 'none';
+  document.getElementById("blur-background").style.display = 'none';
+}
 
